5687f4d98f6ef6e66d835f2447fc968df8460a76
lizye <99151458+lizyejane@users.noreply.github.com>
2024-10-21 18:16:53 +0900

    Add files via upload

---
+++ Python/5_리스트_딕셔너리.py
@@ -0,0 +1,11 @@
+cabinet = {'A-3':'푸','B-100':"피글렛"}
+print(cabinet)
+cabinet["A-3"] = "티거"
+cabinet["C-20"] = "이요르"
+print(cabinet)
+# del cabinet["A-3"]
+# print(cabinet)
+
+print(cabinet.keys())
+print(cabinet.values())
+print(cabinet.items())
